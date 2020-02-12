6ff60d575187f3ab57fef90299aaecf02e90229b
silumi <silumikaundika@gmail.com>
2020-02-12 18:31:58 +0530

    Front end - top navigation

--- Construction-SPA/src/app/app.module.ts
+++ Construction-SPA/src/app/app.module.ts
@@ -6,12 +6,21 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TopnavComponent } from './dashboard/topnav/topnav.component';
+import { SearchComponent } from './dashboard/search/search.component';
+import { NotificationsComponent } from './dashboard/notifications/notifications.component';
+import { NewComponent } from './dashboard/new/new.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    TopnavComponent
+    TopnavComponent,
+    SearchComponent,
+    NotificationsComponent,
+    NewComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
